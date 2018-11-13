7be033ca57a5c23dbe0c97d539cf2687ccde77ce
mattwynyard <mjwynyard@gmail.com>
2018-11-13 14:29:10 +1300

    Bluetooth connect added

--- src/Bluetooth/BluetoothManager.java
+++ src/Bluetooth/BluetoothManager.java
@@ -1,5 +1,6 @@
 package Bluetooth;
 
+import javax.bluetooth.BluetoothStateException;
 //import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DeviceClass;
 import javax.bluetooth.DiscoveryAgent;
@@ -9,16 +10,22 @@ import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
 import java.io.IOException;
 import java.util.Vector;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.bluetooth.DataElement;
 
 public class BluetoothManager implements DiscoveryListener {
 	
 	private LocalDevice mLocalDevice;
-	RemoteDevice remoteDevice;
+	private RemoteDevice mRemoteDevice;
 	private DiscoveryAgent mAgent;
 	//private BluetoothManager bluetoothManager = new BluetoothManager();
 	private static Object lock=new Object();
 	//vector containing the devices discovered
-	private static Vector<RemoteDevice> vecDevices = new Vector();
+	private static Vector<RemoteDevice> mDevices = new Vector();
+	private static String connectionURL = null;
+	//int[] attrIds = { 0x0100 };
 	
 	public BluetoothManager() {
 		
@@ -37,6 +44,7 @@ public class BluetoothManager implements DiscoveryListener {
 		//get devices
 		mAgent = mLocalDevice.getDiscoveryAgent();
 		mAgent.startInquiry(DiscoveryAgent.LIAC, this);
+		//SampleSPPClient client=new SampleSPPClient();
 		
 		try {
 			synchronized(lock){
@@ -50,7 +58,7 @@ public class BluetoothManager implements DiscoveryListener {
 		System.out.println("Device Inquiry Completed. ");
 
 		//print all devices in vecDevices
-		int deviceCount = vecDevices.size();
+		int deviceCount = mDevices.size();
 
 		if(deviceCount <= 0) {
 			System.out.println("No Devices Found .");
@@ -58,10 +66,41 @@ public class BluetoothManager implements DiscoveryListener {
 			//print bluetooth device addresses and names in the format [ No. address (name) ]
 			System.out.println("Bluetooth Devices: ");
 			for (int i = 0; i < deviceCount; i++) {
-				RemoteDevice remoteDevice = (RemoteDevice)vecDevices.elementAt(i);
-				System.out.println((i+1) + ". " + remoteDevice.getBluetoothAddress() + " ("+remoteDevice.getFriendlyName(true) + ")");
+				RemoteDevice remoteDevice = (RemoteDevice) mDevices.elementAt(i);
+				System.out.println((i+1) + ". " + remoteDevice.getBluetoothAddress() + " ("+ remoteDevice.getFriendlyName(true) + ")");
+				
+				if (remoteDevice.getFriendlyName(true).equals("OnSite_BLT_Adapter")) {
+					mRemoteDevice = (RemoteDevice) mDevices.elementAt(i);
+					connect(mRemoteDevice, mAgent, this);
+					
+					//System.out.println("Attempting connection to: "+ remoteDevice.getFriendlyName(true));
+				}
 			}
 		}
+	}
+	
+	public void connect(RemoteDevice remoteDevice, DiscoveryAgent agent, BluetoothManager client) {
+		
+		UUID[] uuidSet = new UUID[1];
+        uuidSet[0]=new UUID("0003000000001000800000805F9B34FB", false);
+        int[] attrIds = { 0x0003 };
+        System.out.println("\nSearching for service...");
+        
+        try {
+			agent.searchServices(attrIds, uuidSet, remoteDevice, client);
+		} catch (BluetoothStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+//        if(connectionURL == null){
+//            System.out.println("Device does not support Simple SPP Service.");
+//            System.exit(0);
+//        }
+
+        //connect to the server and send a line of text
+        
+		
 	}
 	
 	/**
@@ -70,17 +109,40 @@ public class BluetoothManager implements DiscoveryListener {
 	public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
 		System.out.println("Device discovered: "+ btDevice.getBluetoothAddress());
 		//add the device to the vector
-		if(!vecDevices.contains(btDevice)){
-			vecDevices.addElement(btDevice);
-		}
-	}
-	
-	//no need to implement this method since services are not being discovered
-		public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+		if(!mDevices.contains(btDevice)){
+			mDevices.addElement(btDevice);
 		}
 
-		//no need to implement this method since services are not being discovered
+	}
+	
+	//Need to implement this method since services are being discovered
+		public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+
+			System.out.println("Service discovered");
+			System.out.println(servRecord[0].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false));
+			connectionURL = servRecord[0].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+			
+			try {
+				StreamConnection streamConnection = (StreamConnection) Connector.open(connectionURL);
+				if (streamConnection != null) {
+					System.out.println("Connection succesful...");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	        synchronized(lock){
+	            lock.notify();
+	        }
+			
+		}
+
+		//Need to implement this method since services are not being discovered
 		public void serviceSearchCompleted(int transID, int respCode) {
+			synchronized(lock){
+	            lock.notify();
+	        }
 		}
 
 
